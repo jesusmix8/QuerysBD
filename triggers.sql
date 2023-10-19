2545ec93641e958d2d31a9c67e4664b2da26e39d
aquirozc <alejandroqc03@outlook.com>
2023-10-18 20:42:52 -0600

    Cree Trigger para validar correo en tabla Persona

--- triggers.sql
+++ triggers.sql
@@ -1,1 +1,20 @@
 /* Creacion de los triggers  para cada tabla*/ 
+
+/*Triggers tabla persona*/
+
+CREATE OR REPLACE FUNCTION validar_correo()
+  RETURNS TRIGGER 
+  LANGUAGE PLPGSQL
+  AS
+$$
+BEGIN
+	IF NEW.correo LIKE '%@%.%' THEN
+		 RETURN NEW;
+	END IF;
+
+	RETURN NULL;
+END;
+$$
+
+CREATE TRIGGER BEFORE INSERT ON PERSONA FOR EACH ROW EXECUTE FUNCTION validar_correo();
+CREATE TRIGGER BEFORE UPDATE ON PERSONA FOR EACH ROW EXECUTE FUNCTION validar_correo();
